af35b6873e0c66c2b95f2b7d70009176b5f292ac
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2024-09-10 08:11:50 -0900

    added sem_10, hw_10

--- seminars/sem10/ClinicService/ClinicService/Services/Impl/ClientRepository.cs
+++ seminars/sem10/ClinicService/ClinicService/Services/Impl/ClientRepository.cs
@@ -1,32 +1,127 @@
 ﻿using ClinicService.Models;
+using Microsoft.Data.Sqlite;
 
 namespace ClinicService.Services.Impl
 {
     public class ClientRepository : IClientRepository
     {
+        private const string connectionString = "Data Source = clinic.db;";
+
         public int Create(Client item)
         {
-            throw new NotImplementedException();
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                // Прописываем в команду SQL-запрос на добавление данных
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "INSERT INTO clients(Document, SurName, FirstName, Patronymic, Birthday) VALUES(@Document, @SurName, @FirstName, @Patronymic, @Birthday)";
+                command.Parameters.AddWithValue("@Document", item.Document);
+                command.Parameters.AddWithValue("@SurName", item.SurName);
+                command.Parameters.AddWithValue("@FirstName", item.FirstName);
+                command.Parameters.AddWithValue("@Patronymic", item.Patronymic);
+                command.Parameters.AddWithValue("@Birthday", item.Birthday.Ticks);
+                // Подготовка команды к выполнению
+                command.Prepare();
+                // Выполнение команды
+                return command.ExecuteNonQuery();
+            }
         }
 
         public int Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                // Прописываем в команду SQL-запрос на удаление данных
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "DELETE FROM clients WHERE ClientId = @ClientId";
+                command.Parameters.AddWithValue("@ClientId", id);
+                // Подготовка команды к выполнению
+                command.Prepare();
+                // Выполнение команды
+                return command.ExecuteNonQuery();
+            }
         }
 
         public List<Client> GetAll()
         {
-            throw new NotImplementedException();
+            List<Client> list = new List<Client>();
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                // Прописываем в команду SQL-запрос на получение данных
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM clients";
+                // Выполнение команды
+                SqliteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Client client = new Client()
+                    {
+                        ClientId = reader.GetInt32(0),
+                        Document = reader.GetString(1),
+                        SurName = reader.GetString(2),
+                        FirstName = reader.GetString(3),
+                        Patronymic = reader.GetString(4),
+                        Birthday = new DateTime(reader.GetInt64(5))
+                    };
+
+                    list.Add(client);
+                }
+            }
+            return list;
         }
 
         public Client GetById(int id)
         {
-            throw new NotImplementedException();
+            List<Client> list = new List<Client>();
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                // Прописываем в команду SQL-запрос на получение данных
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM clients WHERE ClientId=@ClientId";
+                command.Parameters.AddWithValue("@ClientId", id);
+                // Подготовка команды к выполнению
+                command.Prepare();
+                // Выполнение команды
+                SqliteDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    Client client = new Client()
+                    {
+                        ClientId = reader.GetInt32(0),
+                        Document = reader.GetString(1),
+                        SurName = reader.GetString(2),
+                        FirstName = reader.GetString(3),
+                        Patronymic = reader.GetString(4),
+                        Birthday = new DateTime(reader.GetInt64(5))
+                    };
+                    return client;
+                }
+            }
+            return null;
         }
 
         public int Update(Client item)
         {
-            throw new NotImplementedException();
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                // Прописываем в команду SQL-запрос на обновление данных
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "UPDATE clients Document = @Document, SurName = @SurName, FirstName = @FirstName, Patronymic = @Patronymic, Birthday = @Birthday WHERE ClientId = @ClientId";
+                command.Parameters.AddWithValue("@ClientId", item.ClientId);
+                command.Parameters.AddWithValue("@Document", item.Document);
+                command.Parameters.AddWithValue("@SurName", item.SurName);
+                command.Parameters.AddWithValue("@FirstName", item.FirstName);
+                command.Parameters.AddWithValue("@Patronymic", item.Patronymic);
+                command.Parameters.AddWithValue("@Birthday", item.Birthday.Ticks);
+                // Подготовка команды к выполнению
+                command.Prepare();
+                // Выполнение команды
+                return command.ExecuteNonQuery();
+            }
         }
     }
 }
